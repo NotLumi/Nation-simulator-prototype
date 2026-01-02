70f8c4ae488ca972ee98d46207a423b343050d7d
Lumi <gravesvnt@gmail.com>
2026-01-02 08:30:16 +0000

    Active tiles

--- nation-simulator-prototype/Scripts/main.gd
+++ nation-simulator-prototype/Scripts/main.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 @onready var nationTilemapLayer: TileMapLayer = %NationTilemapLayer
 @onready var evolutionTimer: Timer = %EvolutionTimer
+@onready var updateTimer: Timer = %UpdateTimer
 @onready var simulateButton: Button = %SimulateButton
 @onready var yearLabel: Label = %YearLabel
 @onready var debugLabel: Label = %DebugLabel
 @onready var fpsLabel: Label = %FPSLabel
+@onready var activeTileTilemapLayer: TileMapLayer = %ActiveTileTilemapLayer
 
 
 var evolve : bool = false
@@ -15,16 +17,29 @@ var currentYear : int = 0
 
 #region Data pools
 var nationDictionary : Dictionary[Vector2i, NationResource] = {}
-
+var activeTilesDictionary : Dictionary[Vector2i, NationResource] # Tiles that are updateable
+var activeTilemapArray : Array = []
 #endregion
 
 func _ready() -> void:
 	print(Data.getNations())
 
 func _process(delta: float) -> void:
+	
+	for i in activeTilemapArray:
+		activeTilemapArray.erase(i)
+		activeTileTilemapLayer.erase_cell(i)
+	for i in activeTilesDictionary:
+		activeTileTilemapLayer.set_cell(i, 0, Vector2i.ZERO)
+		if i not in activeTilemapArray:
+			activeTilemapArray.append(i)
+	
+	
+	
 	fpsLabel.text = "FPS: %s" % [Engine.get_frames_per_second()]
 	debugLabel.text = (
-	"Tiles: %s" % [nationDictionary.keys().size()] + "\n" 
+	"Tiles: %s" % [nationDictionary.keys().size()] + "\n" +
+	"Active tiles: %s" % [activeTilesDictionary.size()]
 	)
 	
 func _input(event: InputEvent) -> void:
@@ -40,29 +55,54 @@ func spawnNationAt(pos : Vector2i, nation : NationResource) -> void:
 	nationTilemapLayer.set_cell(pos, 0, nation.atlas)
 	nationDictionary[pos] = nation
 
+func updateActiveTiles() -> void:
+	activeTilesDictionary.clear()
+	
+	for tile in nationDictionary:
+	
+		var emptySurroundingTileArray = getAvailableSurroundingTiles(tile)
+	
+		if emptySurroundingTileArray.size() > 0: # if there is a surrounding tile
+			if !activeTilesDictionary.has(tile): # and surrounding tile not in active
+				activeTilesDictionary[tile] = nationDictionary[tile]
+				
+		else:
+			activeTilesDictionary.erase(tile)
 
 func evolveSimulation() -> void:
-	for nationTile : Vector2i in nationDictionary:
-		var dir = [
-			nationTile + Vector2i.UP,
-			nationTile + Vector2i.DOWN,
-			nationTile + Vector2i.RIGHT,
-			nationTile + Vector2i.LEFT,
-		]
-		
-		var chosenTile : Vector2i = dir.pick_random()
-		
-		if !nationDictionary.has(chosenTile):
-			spawnNationAt(dir.pick_random(), nationDictionary[nationTile])
 
+	for i in activeTilesDictionary:
+		if getAvailableSurroundingTiles(i).size() > 0:
+			if randi() % 2:
+				var emptyTile : Vector2i = getAvailableSurroundingTiles(i).pick_random()
+				spawnNationAt(emptyTile, activeTilesDictionary[i])
+		
 	currentYear += 1
 	yearLabel.text = "Year: %s" % [currentYear]
 	
+func getAvailableSurroundingTiles(tile : Vector2i) -> Array:
+	var newArray : Array[Vector2i] = []
+	
+	var dir = [
+				tile + Vector2i.UP,
+				tile + Vector2i.DOWN,
+				tile + Vector2i.RIGHT,
+				tile + Vector2i.LEFT,
+					]
+					
+	for i in dir:
+		if !nationDictionary.has(i):
+			newArray.append(i)
+	return newArray
 
 func _on_evolution_timer_timeout() -> void:
 	var nations : Dictionary[String, NationResource] = Data.getNations() 
 	evolveSimulation()
 	
+func _on_update_timer_timeout() -> void:
+	updateActiveTiles()
+
+
 func _on_start_mouse_entered() -> void:
 	UIHovered = true
 
@@ -74,9 +114,11 @@ func _on_start_mouse_exited() -> void:
 func _on_simulate_button_pressed() -> void:
 	if evolve:
 		evolutionTimer.stop()
+		updateTimer.stop()
 		evolve = false
 		simulateButton.text = "Start simulation"
 	else:
 		evolutionTimer.start()
+		updateTimer.start()
 		evolve = true
 		simulateButton.text = "End simulation"
