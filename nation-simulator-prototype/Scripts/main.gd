7b069e6e95966d7a1bdec65890618eff48daf1b1
Lumi <gravesvnt@gmail.com>
2026-01-02 07:04:20 +0000

    File structure

---
+++ nation-simulator-prototype/Scripts/main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var nationTilemapLayer: TileMapLayer = %NationTilemapLayer
+
+
+
+
+
+
+func _on_evolution_timer_timeout() -> void:
+	#print("Simulation evolved.")
+	pass
---
+++ nation-simulator-prototype/Scripts/nationResource.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name NationResource
+
+var name : String = "Unknown"
+var atlas : Vector2i = Vector2.ZERO
